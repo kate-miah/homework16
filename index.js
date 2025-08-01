37f8ef4ebb38d52612b605cf6072767ffc1707c6
kate-miah <ekaterin89@gmail.com>
2025-07-31 21:11:09 -0500

    3-4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // 1. 
- let user = {
+
+const user = {
     firstName: 'Kate',
     lastName: 'Miah',
     email: 'email@gmail.com',
@@ -13,10 +14,57 @@
     }
  }
 
-
- //2. Написати функцію, яка приймає об’єкт юзера і виводить вітання типу “Доброго дня, (ім’я_юзера) (прізвище юзера)”
+ // 2. Написати функцію, яка приймає об’єкт юзера і виводить вітання типу “Доброго дня, (ім’я_юзера) (прізвище юзера)”
 
  function sayHello(firstName, lastName) {
     console.log('Доброго дня, ', firstName, lastName);
  }
  sayHello('Kate','Miah');
+
+ // 3. Написати функцію, яка приймає об’єкт і виводить в консоль всі його властивості (за допомогою циклу for..in)
+
+function printUserInfo(user) {
+    for (const key in user) {
+        console.log(`${key}: ${user[key]}`);
+    }
+}
+printUserInfo(user)
+
+// for (const key in newUser) {
+//    console.log(`${key}:`, newUser[key]);
+// }
+
+// 4. Створити об’єкт car (машина) з властивостями:
+ 
+const car = {
+    color: 'white',
+    model: 'XC90',
+    brand: 'Volvo',
+    engineVolume: 2.0,
+    capacity: 5,
+    speed: 0,
+    maxSpeed: 180,
+    accelerate: function(kmh) {
+        const newSpeed = this.speed + kmh;
+        if (newSpeed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        } else {
+            this.speed = newSpeed;
+        }
+        console.log(this.speed);
+    },
+    deaccelerate: function(km) {
+        const newSpeed = this.speed - km;
+        if (newSpeed < 0) {
+            this.speed = 0;
+        } else {
+            this.speed = newSpeed;
+        }
+        console.log(this.speed);
+    },
+    stop: function() {
+        this.speed = 0;
+        console.log(this.speed);
+        return this
+    }
+}
